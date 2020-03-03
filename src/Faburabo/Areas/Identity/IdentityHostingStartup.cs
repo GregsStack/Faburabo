9d8cecbe10a82a7771bb96fb0c5d32add0a7d57a
Gregor Sindl <gregor.sindl@wikifolio.com>
2020-03-03 19:30:26 +0100

    Add TFA qrcode

---
+++ src/Faburabo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Faburabo.Data;
+using Faburabo.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Faburabo.Areas.Identity.IdentityHostingStartup))]
+namespace Faburabo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
